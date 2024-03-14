4d00f254597ba8ee16e2cc4b32edf98802a72884
Vasu Singh <51446047+SinghVasu@users.noreply.github.com>
2024-03-14 05:23:21 +0100

    Update waste_classification_streamlit_app.py

--- waste_classification_streamlit_app.py
+++ waste_classification_streamlit_app.py
@@ -17,7 +17,7 @@ RTC_CONFIGURATION = RTCConfiguration({"iceServers": [{"urls": ["stun:stun.l.goog
 # Load the model
 @st.cache_resource
 def load_model():
-    model_path = 'waste_classifier_model.h5'
+    model_path = 'waste_classifier_model_tf.h5'
     model = tf.keras.models.load_model(model_path)
     return model
 
