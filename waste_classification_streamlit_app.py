755335c711c9874a24b76becb067d61468f9c059
Vasu Singh <51446047+SinghVasu@users.noreply.github.com>
2024-03-14 05:45:14 +0100

    Update waste_classification_streamlit_app.py

--- waste_classification_streamlit_app.py
+++ waste_classification_streamlit_app.py
@@ -21,7 +21,7 @@ def load_model():
     model_path = 'waste_classifier_model_tf.h5'
     model = tf.keras.models.load_model(model_path, compile=False)
     model.compile(optimizer='adam',
-                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  loss=tf.keras.losses.binary_crossentropy,
                   metrics=['accuracy'])
     return model
 
